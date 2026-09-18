f5006f1272cba18ce12804371453b9beb8802967
furukawa <dkszoeke@outlook.de>
2026-09-18 20:24:54 +0200

    feat: add pydantic response schema

--- src/mensapi/api/schemas.py
+++ src/mensapi/api/schemas.py
@@ -1,50 +1,43 @@
-from pydantic import BaseModel
+# Define what data the API can expose. Pydantic then validates that the data matches the definitions.
+from __future__ import annotations
+from datetime import datetime
+
+from pydantic import BaseModel, ConfigDict, Field
 
 
-class Dish(BaseModel):
-    name: str
-    price: dict[str,float]
-    nutrients: dict[str,float]
-    allergens: list[dict[str,str]]
+class DishResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True) # allow reading from objects using dot-notation
 
-    model_config = {
-        "json_schema_extra": {
-            "examples": [
-                {
-                    "day": "Donnerstag",
-                    "description": "Orientalischer Linseneintopf mit Kokosmilch",
-                    "price": {
-                        "Students": 1.80,
-                        "Non-Students": 3.40
-                    },
-                    "nutrients": {
-                        "Protein": 25.81,
-                        "Fat": 13.43,
-                        "Saturated Fat": 9.2,
-                        "kcal": 517.52,
-                        "kJ": 2173.09,
-                        "Carbohydrates": 67.99,
-                        "Salt": 8.43, 
-                        "Sugar": 12.19
-                    },
-                    "allergens": [
-                        {"allergen_id": 16, 
-                         "category": "allergens",
-                         "name": "celery"
-                         }
-                    ]
-                }
-            ]
-        }
-    }
-
-
-class DailyMenu(BaseModel):
+    id: int
     day: str
-    date: str
-    name: list[Dish] 
+    date: datetime
+    name: str
 
+    nutrients: NutrientsResponse
+    prices: PricesResponse
+    allergens: list[AllergensResponse]
 
-class WeeklyMenu(BaseModel):
-    days: list[DailyMenu]
+class PricesResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True) # allow reading from objects using dot-notation
 
+    price_students: float
+    price_non_students: float
+
+class NutrientsResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True) # allow reading from objects using dot-notation
+
+    protein: float
+    fat: float
+    saturated_fat: float
+    kcal: float
+    kJ: float
+    carbohydrates: float
+    salt: float
+    sugar: float
+
+class AllergensResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    allergen_id: int
+    category: str
+    name: str
