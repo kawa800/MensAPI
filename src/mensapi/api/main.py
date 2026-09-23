0e5b6445fc3993466071509b8d9562c113a76b8b
furukawa <dkszoeke@outlook.de>
2026-09-23 15:26:08 +0200

    feat: add search by dish id

--- src/mensapi/api/main.py
+++ src/mensapi/api/main.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, status
 from pydantic import BaseModel
 import datetime as dt
 
@@ -35,6 +35,15 @@ async def get_daily_menu(db: SessionDep) -> list[DishResponse]:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Today's dishes not found.")
 
 
+@app.get("/api/{day}", response_model=DishResponse)
+async def get_weekday_menu(day: str, db: Annotated[Session, Depends(get_db)]) -> DishResponse: 
+    pass
+    # dish = db.get(models.Dish, weekday)
+    # if not dish:
+    #     raise HTTPException(status_code=404, detail="Dish not found")
+    # return dish
+
+
 @app.get("/api/week", response_model=list[DishResponse])
 async def get_weekly_menu(db: SessionDep) -> list[DishResponse]:
     result = db.execute(select(models.Dish))
@@ -45,12 +54,21 @@ async def get_weekly_menu(db: SessionDep) -> list[DishResponse]:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Weekly dishes not found.")
 
 
-        # Read Queryparameter
-# @app.get("/api/{weekday}", response_model=DishResponse)
-# async def get_weekday_menu(weekday: str, db: Annotated[Session, Depends(get_db)]) -> DishResponse: 
-#     dish = db.get(models.Dish, weekday)
-#     if not dish:
-#         raise HTTPException(status_code=404, detail="Dish not found")
-#     return dish
+@app.get("/api/dish/{id}", response_model=list[DishResponse])
+async def get_dish_by_id(id: int, db: SessionDep) -> list[DishResponse]:
+    result = db.execute(select(models.Dish).where(models.Dish.id == id))
+    dishes = result.scalars().all()
+    if dishes:
+        return dishes
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Dish not found.")
 
 
+@app.get("/api/week/{start_date}-{end_date}", response_model=list[DishResponse])
+async def get_weekly_menu(db: SessionDep, start_date: dt.date, end_date: dt.date) -> list[DishResponse]:
+    result = db.execute(select(models.Dish))
+    dishes = result.scalars().all()
+    if dishes:
+        return dishes
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Weekly dishes not found.")
